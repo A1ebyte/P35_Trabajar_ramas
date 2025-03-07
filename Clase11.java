f04b8c568900ce02e4c9d1e7231502eff05543b7
A1ebyte <freddydeandrade54@gmail.com>
2025-03-07 10:50:05 +0100

    commit ramaissue1

---
+++ Clase11.java
@@ -0,0 +1,15 @@
+package es_goya;
+
+public class Clase11
+{
+    public String metodo1() 
+    {
+        return "metodo1";
+    }
+
+    public String metodo2() 
+    {
+        return "metodo2";
+    }
+
+}
